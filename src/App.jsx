c2e952a84b05a04f3614bb21c71e1bfea2299024
Elizaveta <karnizmrkiz@mail.ru>
2022-03-29 15:26:53 +0300

    create: add css module, onClick

--- src/App.jsx
+++ src/App.jsx
@@ -1,6 +1,33 @@
 import { Header } from "./components/Header";
-import { Card } from "./components/Card";
+import { Card } from "./components/Card/Card";
 import { Drawer } from "./components/Drawer";
+
+const DATA_CARD = [
+  {
+    id: 1,
+    title: "Мужские Кроссовки Nike Blazer Mid Suede",
+    price: 12999,
+    imageUrl: "/img/sneakers/img-1.jpg",
+  },
+  {
+    id: 2,
+    title: "Мужские Кроссовки Nike Air Max 270",
+    price: 12999,
+    imageUrl: "/img/sneakers/img-2.jpg",
+  },
+  {
+    id: 3,
+    title: "Мужские Кроссовки Nike Blazer Mid Suede",
+    price: 8499,
+    imageUrl: "/img/sneakers/img-3.jpg",
+  },
+  {
+    id: 4,
+    title: "Кроссовки Puma X Aka Boku Future Rider",
+    price: 8999,
+    imageUrl: "/img/sneakers/img-4.jpg",
+  },
+];
 
 function App() {
   return (
@@ -16,10 +43,15 @@ function App() {
           </div>
         </div>
         <div className="d-flex">
-          <Card />
-          <Card />
-          <Card />
-          <Card />
+          {DATA_CARD.map(({ title, imageUrl, price, id }) => (
+            <Card
+              key={id}
+              price={price}
+              title={title}
+              imageUrl={imageUrl}
+              onClick={() => console.log(price)}
+            />
+          ))}
         </div>
       </div>
     </div>
--- src/components/Card/Card.js
+++ src/components/Card/Card.js
@@ -1,22 +1,24 @@
-export function Card() {
+import styles from './Card.module.scss'
+
+export function Card({ title, price, imageUrl, onClick }) {
   return (
-    <div className="card">
-      <div className="favorite">
+    <div className={styles.card}>
+      <div className={styles.favorite}>
         <img src="/img/heart-unliked.svg" alt="unliked" />
       </div>
       <img
         width={133}
         height={112}
-        src="/img/sneakers/img-1.jpg"
+        src={imageUrl}
         alt="sneakers"
       />
-      <h5>Мужские Кроссовки Nike Blazer Mid Suede</h5>
+      <h5>{title}</h5>
       <div className="d-flex justify-between align-center">
         <div className="d-flex flex-column">
           <span>Цена:</span>
-          <b>12 999 р.</b>
+          <b>{price} руб.</b>
         </div>
-        <button className="button">
+        <button className={styles.button} onClick={onClick}>
           <img width={11} height={11} src="/img/plus.svg" alt="plus" />
         </button>
       </div>
